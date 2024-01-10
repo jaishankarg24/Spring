44a83f94a98cb4201ebaa54610a6d49a13a76ef0
jaishankar <jaishankarg24@gmail.com>
2024-01-10 21:21:55 +0530

    @PostConstruct & @PreDestroy annotations for a Stereotype Annotation.

---
+++ example6/src/main/java/com/js/beans/Vehicle.java
@@ -0,0 +1,35 @@
+package com.js.beans;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    public void initialize() {
+        this.name = "Honda";
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println(
+                "Destroying Vehicle Bean");
+    }
+
+    public void printHello(){
+        System.out.println(
+                "Printing Hello from Component Vehicle Bean");
+    }
+}
