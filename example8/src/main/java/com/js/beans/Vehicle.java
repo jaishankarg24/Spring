fbd9fb5d8f5d42c7bb2044898b6e6ea590964b7a
jaishankar <jaishankarg24@gmail.com>
2024-01-13 12:09:13 +0530

    Adding New Beans using XML Config.

---
+++ example8/src/main/java/com/js/beans/Vehicle.java
@@ -0,0 +1,20 @@
+package com.js.beans;
+
+
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello(){
+        System.out.println(
+                "Printing Hello from Component Vehicle Bean");
+    }
+}
